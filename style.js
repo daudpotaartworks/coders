a7a7933e0cab45506e384402190a52f7269fe74a
daudpotaartworks <daudpotaartworks@gmail.com>
2022-03-12 20:54:38 +0500

    my ui code

---
+++ style.js
@@ -0,0 +1,62 @@
+
+{
+    const list = document.querySelectorAll('.list');
+    function activeLink(){
+        list.forEach((item) => 
+        item.classList.remove('active'));
+        this.classList.add('active');
+    }
+    list.forEach((item) => 
+    item.addEventListener('click', activeLink));
+}
+
+
+
+// %%%%%%%%%%%% SCROLL TO TOP BUTTON %%%%%%%%%%%%%%%%
+
+{
+    var mybutton = document.getElementById("myBtn");
+    window.onscroll = function() {scrollFunction()};
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+}
+
+
+// %%%%%%%%%%%%%%%%%%%% SLIDE SHARE %%%%%%%%%%%%%%%%%%
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
